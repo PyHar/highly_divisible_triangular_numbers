d95313349440586346e8e1af8e12430877f06412
Pyry <kenfaxx@gmail.com>
2022-11-20 15:43:02 +0200

    new code

--- testing.py
+++ testing.py
@@ -1,16 +1,28 @@
-import math
+#first we import what we need
+from math import *
+import sys
 
+#stdout so we can print result into the file
+original_stdout = sys.stdout
+#define our variable
 def triangular_numbers (n):
     a = 1
     b = 1
-
+#for loop
     for i in range (1, n + 1):
-            f=open ('output.txt', 'wt' )
-            print(b, end = ' ', file = f)
-            print(b, end = ' ')
-            a = a + 1
-            b = a + a
+        #print to console
+        print (b, end = ' ')
+        with open('output.txt', 'a') as f:
+            sys.stdout = f
+            #print to output.txt file
+            print (b, end = ' ')
+            sys.stdout = original_stdout
 
-n = 8
+
+        a = a + 1
+        b = a + a
+#here we can define how many numbers we want to print out
+n = 10
 triangular_numbers(n)
+
 
