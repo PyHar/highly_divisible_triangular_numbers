e6df3515d428518d14e5efe50b40d05d4724e633
Pyry <kenfaxx@gmail.com>
2022-11-25 10:15:03 +0200

    new code

--- highly_divisible_numbers.py
+++ highly_divisible_numbers.py
@@ -1,52 +1,48 @@
-
+#first we import what we need
 from math import *
+import sys
+original_stdout = sys.stdout #stdout so we can print result into the file
+def triangleNumbers(n): #define our variable
+#for loop
+    for i in range (1, n + 1):
+        #print to console
+        print (i*(i+1)//2,end=' ')
+        with open ('output.txt', 'a') as f:
+            sys.stdout = f
+            #print to output.txt file
+            print (i*(i+1)//2,end=' ')
+            sys.stdout = original_stdout
+n = 10 # how many triangular numbers we want to print
+triangleNumbers(n)
 
-# Function to calculate the number of divisors of integer n
-def divisors(n):
-    limit = int(sqrt(n))
-    divisors_list = []
-    for i in range(1, limit+1, 1):
-        if n % i == 0:
-            divisors_list.append(i)
-            if i != n/i:
-                divisors_list.append(n/i)
-    return len(divisors_list)
-
-
-def isTriangleNumber(n):
-    a = int(sqrt(2*n))
-    return 0.5*a*(a+1) == n
-
-
-def lastTerm(n):
-    if isTriangleNumber(n):
-        return int(sqrt(2*n))
-    else:
-        return None
-
-check = 2**4 * 3**4 * 5**4 * 7 * 11
-
-
-while not isTriangleNumber(check):
-    check += 1
-
-
-seriesLastTerm = lastTerm(check)
-
-
-while divisors(check) <= 500:
-
-    check += (seriesLastTerm + 1)
-    seriesLastTerm += 1
-print (check)
-f = open('results.txt', 'w')
-
-
-def triangle_series ( n ):
-
-	for i in range(1, n + 1):
-		print( i*(i+1)//2, end=' ')
-
-n = 1
-
-triangle_series(n)
+def Divisors(n = 0, agg = 0):
+    while True:
+        n += 1
+        agg += n
+        #pass function
+        yield agg
+#define number of divisors
+def number_of_divisors(n):
+    num_factors = 2
+    #square number
+    sq = int(n ** 0.5)
+    for candidate in range(2, sq):
+        if n % candidate == 0:
+            num_factors += 2
+    if sq * sq == n:
+        num_factors -= 1
+    return num_factors
+#use vectors to find triangle numbers
+if __name__ == "__main__":
+    current_magnitude = 10
+    for num in Divisors():
+        #here we can specify how many divisors we want for the number to have
+        if number_of_divisors(num) >= 3:
+             #print to console
+            print("Number is: " + str(num))
+            with open('output2.txt', 'a') as f:
+                sys.stdout = f
+                #print to output2.txt
+                print("Number is: " + str(num))
+                sys.stdout = original_stdout
+            exit(0)
